1f7ce0b49cff002053d2290b20b6c06bac84c4c3
ItzHaru <ruzena05perinova@gmail.com>
2024-01-06 15:18:13 +0100

    color

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -706,6 +706,7 @@ export interface ApiCategoryCategory extends Schema.CollectionType {
       'manyToMany',
       'api::subject.subject'
     >;
+    Color: Attribute.String;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
